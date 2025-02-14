e44358064f292c079caf4284e13adeae2cd1e39d
alirezz2020 <alirezz2020.py@gmail.com>
2025-02-14 16:52:02 +0330

    added daphne

--- ChatProject/asgi.py
+++ ChatProject/asgi.py
@@ -1,4 +1,3 @@
-# ChatProject/asgi.py
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -6,7 +5,6 @@ from django.core.asgi import get_asgi_application
 import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChatProject.settings")
-
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
--- ChatProject/settings.py
+++ ChatProject/settings.py
@@ -9,6 +9,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    'daphne',
     # Default apps...
     'django.contrib.admin',
     'django.contrib.auth',
